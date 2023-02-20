c8fb5e5dfb919bb3bfc77e65661ca5dfdab28a80
MIGUEL GONCALVES <miguelrgoncalves97@gmail.com>
2023-02-20 14:23:06 +0000

    2nd done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:questions]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
+  end
+end
+
+
+\
